e2b82e26e7d8f698a299a82e61a96fa3ffb34861
Alex Espinoza <aespinoza@aspria.net>
2013-08-11 14:41:54 -0400

    Adjusted page grid for character dialogue, styled text

--- game.js
+++ game.js
@@ -10,26 +10,16 @@ $(function() {
 		narTxt.hide();
 		narTxt.text("Time.");
 		narTxt.fadeIn(800).delay(1500).fadeOut(800, function() {
-			$(this).text("It's odd how it works out.");
+			$(this).text("It's funny how it works out.");
 		}).delay(500).fadeIn(800).delay(2500).fadeOut(800, function() {
 			$(this).text("You always think you have so much of it.");
 		}).delay(500).fadeIn(800).delay(2500).fadeOut(800, function() {
-			$(this).text("But then one day you wake up and it's gone.");
+			$(this).text("But then one day you wake up and realize that it's gone.");
 		}).delay(500).fadeIn(800).delay(2500).fadeOut(800, function() {
-			$(this).text("You never really had it.");
+			$(this).text("You never really had it in the first place.");
 		}).delay(500).fadeIn(800).delay(2500).fadeOut(800, function() {
 			$(this).text("Time had you.");
 		}).delay(500).fadeIn(800).delay(2500).fadeOut(800);
-		// $(".narrator-text").text("It's odd how it works out.");
-		// $(".narrator-text").fadeIn(800).delay(3000).fadeOut(800).delay(1000);
-		// $(".narrator-text").text("You always think you have so much of it.");
-		// $(".narrator-text").fadeIn(800).delay(3000).fadeOut(800).delay(1000);
-		// $(".narrator-text").text("But then one day you wake up and it's gone.");
-		// $(".narrator-text").fadeIn(800).delay(3000).fadeOut(800).delay(1000);
-		// $(".narrator-text").text("You never really had it.");
-		// $(".narrator-text").fadeIn(800).delay(3000).fadeOut(800).delay(1000);
-		// $(".narrator-text").text("Time had you.");
-		// $(".narrator-text").fadeIn(800).delay(3000).fadeOut(800).delay(1000);
 	}
 
 	$('.digital-clock').click(function() {
